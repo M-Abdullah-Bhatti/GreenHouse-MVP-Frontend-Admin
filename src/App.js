979b7d691c1de396de1600f381b70e64736d97af
ShayanJamil519 <shayanjamil500@gmail.com>
2023-09-07 23:45:49 +0500

    Login API Integrated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Route, Routes } from "react-router-dom";
 import Reports from "./Pages/Reports";
 import Header from "./Components/Shared/Header";
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+       <ToastContainer position="top-right" autoClose={3000} />
       {openLoginModal && <Login />}
       <Routes>
         <Route path="/" element={<Reports />}></Route>
--- src/Components/Shared/Login.jsx
+++ src/Components/Shared/Login.jsx
@@ -1,13 +1,40 @@
 import { useStepsContext } from "../../Context/StateContext";
+import { useFormik } from "formik";
+import { loginModalScehma } from "../../validation-schema";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const { openLoginModal, setOpenLoginModal } = useStepsContext();
 
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema: loginModalScehma,
+
+      onSubmit: async (values) => {
+        try {
+          const { data } = await axios.post(
+            "http://localhost:5000/api/regulator/login",
+            values
+          );
+
+          toast.success("Logged in Successfully");
+          setOpenLoginModal(!openLoginModal);
+        } catch (err) {
+          toast.error(err?.response?.data?.message);
+          // console.error(err);
+        }
+      },
+    });
+
   return (
-    <div
-      style={{ zIndex: 1000, top: "80px" }}
-      className="fixed left-0 right-0 w-full px-4 overflow-x-hidden overflow-y-auto  h-full bg-[rgb(0,0,0,0.75)]"
-    >
+    <div className="fixed z-20 left-0 top-0 right-0 w-full px-4 overflow-x-hidden overflow-y-auto  h-full bg-[rgb(0,0,0,0.75)]">
       <div className="relative w-full ">
         <div className="flex items-start justify-between mt-4">
           <button
@@ -33,34 +60,72 @@ const Login = () => {
         </div>
       </div>
       <div className="flex justify-center py-1 w-full">
-        <form
+        <div
           //   onSubmit={handleSubmit}
-          className="bg-[#FCF5F3] w-[98%] md:w-[60%] rounded-3xl py-4 px-3 sm:p-6 sm:px-7 hide_scrollbar"
+          className="bg-[#fff] w-[98%] lg:w-[40%] rounded-3xl py-4 px-3  sm:p-6 sm:px-7 md:py-10 md:px-16 "
         >
-          <h3 className="text-darkblue mb-10 mt-5 text-2xl sm:text-3xl md:text-4xl font-[600] text-center font-BalsamiqSans">
-            Green Washing
-          </h3>
+          <div>
+            <div className="flex justify-center items-center ">
+              <img src="./assets/__logo.png" alt="logo" className="w-[80px]" />
+              <div>
+                <h1 className="text-lg font-bold leading-5">
+                  Greenwashing <br /> Identifier
+                </h1>
+                <p className="text-sm text-[#626262] ">By ImpactScope</p>
+              </div>
+            </div>
+            <h3 className="uppercase text-darkblue mt-5 text-2xl sm:text-3xl md:text-4xl font-[600] text-center">
+              Welcome to GWI
+            </h3>
 
-          <p
-            className="font-BalsamiqSans text-center text-[#626262] relative"
-            style={{ bottom: "20px" }}
-          >
-            Identifier
-          </p>
-
-          {/* Button */}
-          {/* <div className="bg-[#fff] mt-10 flex mb-10"> */}
-
-          <div className="bg-[#fcf5f3] mt-10 flex mb-10">
-            <button
-              type="submit"
-              //   onClick={openToggle === false ? onClick : null}
-              //   disabled={loading}
-              // disabled={loading}
-              className="bg-orange cursor-pointer w-full relative h-[64px]  text-white border-2 text-center border-orange font-BalsamiqSans rounded-full  py-3 text-lg font-medium"
-            ></button>
+            <p className="font-BalsamiqSans text-center text-[#626262] text-lg mt-3 ">
+              Sign in to get started
+            </p>
           </div>
-        </form>
+
+          <form onSubmit={handleSubmit} className="w-full mt-5 space-y-3">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Email"
+              className="p-4 bg-[#f5f4f4] rounded-lg border-none focus:outline-none w-full"
+            />
+
+            {errors.email && touched.email ? (
+              <p className="text-sm text-[#ff0000]">{errors.email}</p>
+            ) : null}
+
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder="Password"
+              className="p-4 bg-[#f5f4f4] border-none rounded-lg focus:outline-none w-full"
+            />
+            {errors.password && touched.password ? (
+              <p className="text-sm  text-[#ff0000]">{errors.password}</p>
+            ) : null}
+
+            <div className="!mt-10">
+              <button
+                type="submit"
+                //   onClick={openToggle === false ? onClick : null}
+                //   disabled={loading}
+                // disabled={loading}
+                className="bg-[#3FDD78] cursor-pointer w-full relative h-[64px]  text-white text-center  rounded-md  py-3 text-lg font-medium"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
