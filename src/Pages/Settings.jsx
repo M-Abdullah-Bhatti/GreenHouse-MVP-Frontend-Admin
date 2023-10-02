fc4eed27dc5db95a8f3858ceb45275d6c6b794ba
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-10-02 22:55:06 +0500

    get and update api integrated

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -1,13 +1,38 @@
 /* eslint-disable no-template-curly-in-string */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-
-const defaultPrompt="Identify any inconsistencies for ${targetCompanyName} within the data across 'InsigAI,' 'Twitter,' and 'Carbon offsets' sheets. Report conflicting details or misalignments between these sources, if any, in a concise manner. Keep the response within 12 lines."
+const defaultPrompt =
+  "Identify any inconsistencies for ${targetCompanyName} within the data across 'InsigAI,' 'Twitter,' and 'Carbon offsets' sheets. Report conflicting details or misalignments between these sources, if any, in a concise manner. Keep the response within 12 lines.";
 
 const Settings = () => {
+  const [promptText, setPromptText] = useState(defaultPrompt);
 
-    const [promptText, setPromptText] = useState(defaultPrompt); 
+  useEffect(() => {
+    const loadData = async () => {
+      const gptPrompt = await axios.get(`http://localhost:5000/api/prompt`);
+      console.log("--", gptPrompt?.data?.result?.prompt);
+      setPromptText(gptPrompt?.data?.result?.prompt);
+    };
+    loadData();
+  }, []);
 
+  const handleSubmit = async () => {
+    console.log("promptText", promptText);
+    const res = axios
+      .put("http://localhost:5000/api/prompt/updatePrompt", {
+        prompt: promptText,
+      })
+      .then((res) => {
+        console.log("res: ", res);
+        toast.success("Prompt updated successfully");
+      })
+      .catch((err) => {
+        console.log("err: ", err);
+        toast.error(err);
+      });
+  };
 
   return (
     <div className="w-[90%] mx-auto my-10">
@@ -21,17 +46,27 @@ const Settings = () => {
       <div className="w-1/2 ml-0 mt-16">
         <div className="flex justify-between items-center mb-2">
           <h1 className="font-bold text-lg">Prompt</h1>
-          <h1 onClick={()=>setPromptText(defaultPrompt)} className="text-[#3FDD78] font-bold cursor-pointer">Restore to default</h1>
+          <h1
+            onClick={() => setPromptText(defaultPrompt)}
+            className="text-[#3FDD78] font-bold cursor-pointer"
+          >
+            Restore to default
+          </h1>
         </div>
 
         <div>
           <textarea
             type="text"
             value={promptText}
-            onChange={(e)=>setPromptText(e.target.value)}
+            onChange={(e) => setPromptText(e.target.value)}
             className="scrollbar w-full p-4 text-[15px] rounded-lg border-[1px] border-[#E8ECEF] min-h-[200px] focus:border-gray-400 focus:border-opacity-50 focus:outline-none"
           />
-          <button className="py-2 px-6 text-[#fff] rounded-lg font-semibold mt-3 bg-[#3FDD78] border-none outline-none">Update</button>
+          <button
+            className="py-2 px-6 text-[#fff] rounded-lg font-semibold mt-3 bg-[#3FDD78] border-none outline-none"
+            onClick={handleSubmit}
+          >
+            Update
+          </button>
         </div>
       </div>
     </div>
